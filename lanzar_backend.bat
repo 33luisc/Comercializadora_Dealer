88f2e160666bf160357714f8ff91bb52ced049b9
18luisc <lucero.luis@correounivalle.edu.co>
2026-08-29 18:44:14 -0500

    Inicio
    Desplegar en otro PC

--- lanzar_backend.bat
+++ lanzar_backend.bat
@@ -1,4 +1,4 @@
 @echo off
 title Backend Server
-cd backend
-npm run dev
+cd /d "%~dp0backend"
+"%~dp0node_bin\node.exe" "%~dp0node_bin\node_modules\npm\bin\npm-cli.js" run dev
--- lanzar_frontend.bat
+++ lanzar_frontend.bat
@@ -1,4 +1,4 @@
 @echo off
 title Frontend App
-cd frontend
-npm run dev
+cd /d "%~dp0frontend"
+"%~dp0node_bin\node.exe" "%~dp0node_bin\node_modules\npm\bin\npm-cli.js" run dev
